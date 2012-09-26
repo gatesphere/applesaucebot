102cbb3b96510348ae3ae5c57110ecf8c955a72f
root <root@suspended-chord.info>
2012-09-26 19:28:57 -0400

    Flood protection on define module.

--- modules/define.py
+++ modules/define.py
@@ -6,6 +6,7 @@ def define(self, username, cmdtime, command, args):
     return '\\u00' + match.group()[2:]
  
   if args:
+    defn_count = 3
     for word in args:
       url = 'http://www.google.com/dictionary/json?callback=a&q=' + word + '&sl=en&tl=en&restrict=pr,de&client=te' 
       content = urllib.urlopen(url).read()[2:-10]
@@ -18,6 +19,8 @@ def define(self, username, cmdtime, command, args):
             if bunch['entries'][i]['type'] != 'meaning': continue          
             meaning = bunch['entries'][i]['terms'][0]['text']
             self.protocol.privmsg(self.channel, "%s - %s (from google)" % (word, meaning))
+            defn_count -= 1
+            if defn_count == 0: return
             time.sleep(1)
       if 'webDefinitions' in data:
         for entry in data['webDefinitions'][0]['entries']:
@@ -25,7 +28,13 @@ def define(self, username, cmdtime, command, args):
           source = entry['terms'][1]['text']
           source = source[source.find(">")+1:source.rfind("<")]
           self.protocol.privmsg(self.channel, "%s - %s (from %s)" % (word, meaning, source))      
+          defn_count -= 1
+          if defn_count == 0: return
           time.sleep(1)
+      if defn_count == 3:
+        self.protocol.privmsg(self.channel, "I couldn't find a definition for %s." % word)
+  else:
+    self.protocol.privmsg(self.channel, "%s, I need at least one word to define!" % username)
 
 self.register_action('define', define)
 
--- modules/hello.py
+++ modules/hello.py
@@ -1,8 +1,8 @@
-def hello(self, username, cmdtime, command, args):
-  """ greet the user """
-  self.protocol.privmsg(self.channel, "Hello there, %s!" % username)
+#def hello(self, username, cmdtime, command, args):
+  #""" greet the user """
+  #self.protocol.privmsg(self.channel, "Hello there, %s!" % username)
 
-self.register_action('hello', hello)
-self.register_action('hai?', hello)
-self.register_action('hai', hello)
-self.register_action('herro', hello)
+#self.register_action('hello', hello)
+#self.register_action('hai?', hello)
+#self.register_action('hai', hello)
+#self.register_action('herro', hello)
