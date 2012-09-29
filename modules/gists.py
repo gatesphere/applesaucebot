6b067f691a3429a2d29e14857a317b5553ef7df5
root <root@suspended-chord.info>
2012-09-29 14:45:55 -0400

    gists module

---
+++ modules/gists.py
@@ -0,0 +1,33 @@
+def gists(self, username, cmdtime, command, args):
+  import json, urllib, binascii
+
+  def asciirepl(match):
+    s = match.group()
+    return '\\u00' + match.group()[2:]
+ 
+  if args:
+    gist_count = 3
+    args = " ".join(args)
+    url = 'https://api.github.com/users/' + args + '/gists'
+    content = urllib.urlopen(url).read()
+    reg = re.compile(r'\\x(\w{2})')
+    ascii_string = reg.sub(asciirepl, content).decode('ascii', 'ignore')
+    data = json.loads(ascii_string)
+    if str(data).find('Not Found') != -1:
+      self.protocol.privmsg(self.channel, "I couldn't find any gists for %s." % args)
+      return
+    for gist in data:
+      g_description = gist['description']
+      g_url = gist['html_url']
+      self.protocol.privmsg(self.channel, "%s - %s" % (g_description, g_url))
+      gist_count -= 1
+      if gist_count == 0:
+        return
+      time.sleep(1)
+    if gist_count == 3:
+      self.protocol.privmsg(self.channel, "I couldn't find any gists for %s." % args)
+  else:
+    self.protocol.privmsg(self.channel, "%s, I need a username to search!" % username)
+
+self.register_action('gists', gists)
+
